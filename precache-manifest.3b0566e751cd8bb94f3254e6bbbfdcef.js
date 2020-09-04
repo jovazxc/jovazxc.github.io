e0182427e81a2ba93825feceaa001bb4eaa9e65f
Gogs <gogs@fake.local>
2020-09-03 18:20:20 -0600

    recargar button

---
+++ precache-manifest.3b0566e751cd8bb94f3254e6bbbfdcef.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bb04082f792a54ddab3ef5a059f255f1",
+    "url": "/index.html"
+  },
+  {
+    "revision": "13c0ccbb00a5cdce997a",
+    "url": "/static/css/main.ae605601.chunk.css"
+  },
+  {
+    "revision": "c24f4e9653e5777b381c",
+    "url": "/static/js/2.876ff5ef.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.876ff5ef.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "13c0ccbb00a5cdce997a",
+    "url": "/static/js/main.b2ced5e6.chunk.js"
+  },
+  {
+    "revision": "7fa5f21c7716c02dca13",
+    "url": "/static/js/runtime-main.ea50f2c6.js"
+  },
+  {
+    "revision": "181ef9a7aee45c119e68f294e8426a3a",
+    "url": "/static/media/harry.181ef9a7.ttf"
+  },
+  {
+    "revision": "36e5b8f0842e293e2a85a939151f20dd",
+    "url": "/static/media/stark.36e5b8f0.otf"
+  }
+]);
